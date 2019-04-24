f3de184f61a7f26699a0cdea13a27999cf78c2e0
Singh <Manvendra.Singh@aristocrat.com>
2019-04-24 13:32:16 +0530

    new files

---
+++ Promises1.ts
@@ -0,0 +1,33 @@
+const promise = new Promise(function(resolve,reject)
+{
+    let rt = function(){
+        console.log("Hello .... I am executer");
+        return true;
+    }
+   // console.log(rt);
+    if (rt())
+    {
+        console.log("--> if part...")
+        resolve("Success...")
+    }
+    else
+    {
+        console.log("--> else part")
+        reject("failure...")
+    }
+})
+
+let res = function(result){
+    console.log(result)
+}
+
+let rej = function(result)
+{
+    console.log(result);
+}
+
+promise.then(res).catch(rej)
+
+//promise.then(res).catch(rej).finally()
+//finally is not working in ts code
+
